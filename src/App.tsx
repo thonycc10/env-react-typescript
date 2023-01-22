9cd3b6764f052c327e6eef9daf212c4099389787
Anthony Carrasco <thony10.rc.c@gmail.com>
2023-01-22 00:39:38 -0500

    add file .env

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    // npm i --save-dev dotenv
+    // npm i --save-dev env-cmd
+    console.log(process.env.REACT_APP_ENV);
+    console.log(process.env.REACT_APP_SECRET_NAME);
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        Soy enviroment - {process.env.REACT_APP_ENV}
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
